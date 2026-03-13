f3d743dbdbba006432600c417346f6fa83ecae91
Sebastian Vidal Aedo <sebastianvidalaedo@gmail.com>
2026-03-13 12:48:23 -0300

    Initial Commit

---
+++ FocusBuddy/FocusBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusBuddyApp.swift
+//  FocusBuddy
+//
+//  Created by Sebastián Vidal Aedo on 13-03-26.
+//
+
+import SwiftUI
+
+@main
+struct FocusBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
